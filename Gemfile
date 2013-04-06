87ca88e888ee9c8e39dafa2709c0d9772365839d
Oleg Potapov <oleg0potapov@gmail.com>
2013-04-07 00:51:49 +0400

    Add zurb-foundation

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source :rubygems
 gem 'sinatra'
 gem 'grit'
 gem 'slim'
+gem 'compass'
+gem 'zurb-foundation', '=3.2.5'
--- zitgit.rb
+++ zitgit.rb
@@ -1,13 +1,31 @@
 require 'sinatra/base'
 require 'grit'
 require 'slim'
+require 'zurb-foundation'
+require 'compass'
 
 class Zitgit < Sinatra::Base
+  configure do
+    set :scss, Compass.sass_engine_options
+    set :views, ['views', 'scss']
+  end
+
+  helpers do
+    def find_template(views, name, engine, &block)
+      Array(views).each { |v| super(v, name, engine, &block) }
+    end
+  end
+
   get '/' do
     repo = Grit::Repo.new('')
     current_branch = Grit::Head.current(repo)
     commits = repo.commits(current_branch.name, 200)
     slim :index, :locals => { current_branch: current_branch, commits: commits }
+  end
+
+  get "/stylesheets/*.css" do |path|
+    content_type "text/css", charset: "utf-8"
+    scss :"#{path}"
   end
 
   run! if app_file == $0
