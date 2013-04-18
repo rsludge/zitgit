a7c82d3d2e308a8bf7e3bf50e89db50d9135ba84
Oleg Potapov <oleg0potapov@gmail.com>
2013-04-18 21:23:39 +0400

    change active commit on branch change

--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,22 @@
+var ChangeCommit = function($commit){
+    var $target_commit = $commit.clone();
+    $('table.twelve tr.selected').removeClass('selected');
+    $commit.parents('tr').addClass('selected');
+    $('.show_commit').html($target_commit);
+}
+
 $(function(){
     $('.top-bar .dropdown li a').on('click', function(e){
         e.preventDefault();
         var $link = $(this);
         $.get($link.attr('href'), function(data){
             $('.current_branch').text($link.text());
-            $('table.twelve').replaceWith(data);
+            $('table.twelve').replaceWith(data);            
+            ChangeCommit($('table.twelve tbody tr:first .commit'));
         });
     })
 
-    $('table.twelve tr').on('click', function(e){
-        $('table.twelve tr.selected').removeClass('selected');
-        $(this).addClass('selected');
-        $('.show_commit').html($(this).find('.commit').clone());
+    $('.history').on('click', 'table.twelve tr', function(e){
+        ChangeCommit($(this).find('.commit'));
     })
 });
