60496b8db5899350926a3aba6b8ded8f5aec3078
Oleg Potapov <oleg0potapov@gmail.com>
2013-04-29 22:27:41 +0400

    some refactoring

--- lib/zitgit.rb
+++ lib/zitgit.rb
@@ -32,18 +32,15 @@ module Zitgit
 
     get '/' do
       repo = Grit::Repo.new('.')
-      current_branch = Grit::Head.current(repo)
-      commits = repo.commits(current_branch.name, 200)
-      repo_name = File.basename(repo.working_dir)
+      @current_branch = Grit::Head.current(repo)
+      @commits = repo.commits(@current_branch.name, 200)
+      @last_commit = @commits[0]
+      @repo_name = File.basename(repo.working_dir)
       @branches = repo.heads    
       @remotes = repo.remotes
       @tags = repo.tags
-      slim :index, :locals => { 
-        current_branch: current_branch, 
-        commits: commits, 
-        repo_name: repo_name,
-        last_commit: commits[0]
-      }
+      @index = repo.index
+      slim :index
     end
 
     get "/ref/:ref_name" do |ref_name|
